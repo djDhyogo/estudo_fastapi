d86c2ae47aee8eabaedaa0eb464dd26a659e5ffd
DjDhyogo <dyogo_leal@hotmail.com>
2025-04-08 16:43:08 -0300

    Adicionada a primeira migração com Alemb ic. Criada tabela de usuários.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,23 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fast_zero.app import app
+from fast_zero.models import table_registery
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine("sqlite:///:memory:")
+    table_registery.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registery.metadata.drop_all(engine)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,21 @@
 # tests/test_user.py
+from sqlalchemy import select
+
 from fast_zero.models import User
 
 
-def test_create_user():
+def test_create_user(session):
     user = User(
-        username='Dhyogo',
-        email='dhyogo@example',
-        password='1234',
+        username="Dhyogo",
+        email="dhyogo@example",
+        password="1234",
     )
 
+    session.add(user)
+    session.commit()
+    result = session.scalar(select(User).where(User.email == "dhyogo@example"))
+
+    assert result.email == "dhyogo@example"
+
     # escreva o assert
-    assert user.username == 'Dhyogo'
+    assert user.id == 1
